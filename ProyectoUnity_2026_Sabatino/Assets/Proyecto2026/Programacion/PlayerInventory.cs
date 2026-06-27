2a7dd482d498dab0855a7f9c1d2174a237022112
Asriel-Ivich <perez.mendoza0288@gmail.com>
2026-06-27 04:00:50 -0600

    UI responde a cantidades

--- ProyectoUnity_2026_Sabatino/Assets/Proyecto2026/Programacion/PlayerInventory.cs
+++ ProyectoUnity_2026_Sabatino/Assets/Proyecto2026/Programacion/PlayerInventory.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
@@ -7,13 +8,22 @@ public class PlayerInventory : MonoBehaviour
     public int flechas = 0;
     public int antorchas = 0;
 
+    [Header("UI TextMeshPro")]
+    public TMP_Text pocionesText;
+    public TMP_Text flechasText;
+    public TMP_Text antorchasText;
+
     [Header("Pociones")]
     public int curacionPocion = 30;
     public HealthSystem vidaJugador;
 
     [Header("Antorcha")]
     public GameObject antorchaObjeto;
-    
+
+    void Start()
+    {
+        ActualizarUI();
+    }
 
     void Update()
     {
@@ -22,14 +32,19 @@ public class PlayerInventory : MonoBehaviour
 
     private void UsarPocion()
     {
-        // Tecla Q para usar pocion
+        // Usa pocion con Q
         if (Input.GetKeyDown(KeyCode.Q))
         {
             if (pociones > 0)
             {
                 pociones--;
 
-                vidaJugador.CurarVida(curacionPocion);
+                if (vidaJugador != null)
+                {
+                    vidaJugador.CurarVida(curacionPocion);
+                }
+
+                ActualizarUI();
 
                 Debug.Log("Usaste una pocion. Pociones restantes: " + pociones);
             }
@@ -40,11 +55,15 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
+
     public bool UsarFlecha()
     {
         if (flechas > 0)
         {
             flechas--;
+
+            ActualizarUI();
+
             Debug.Log("Flecha usada. Flechas restantes: " + flechas);
             return true;
         }
@@ -57,19 +76,38 @@ public class PlayerInventory : MonoBehaviour
     public void AgregarPociones(int cantidad)
     {
         pociones += cantidad;
+        ActualizarUI();
     }
 
     public void AgregarFlechas(int cantidad)
     {
         flechas += cantidad;
+        ActualizarUI();
     }
 
     public void AgregarAntorchas(int cantidad)
     {
         antorchas += cantidad;
+        ActualizarUI();
     }
 
     #region UI
-    
+    public void ActualizarUI()
+    {
+        if (pocionesText != null)
+        {
+            pocionesText.text = pociones.ToString();
+        }
+
+        if (flechasText != null)
+        {
+            flechasText.text = flechas.ToString();
+        }
+
+        if (antorchasText != null)
+        {
+            antorchasText.text = antorchas.ToString();
+        }
     #endregion UI
+    }
 }
